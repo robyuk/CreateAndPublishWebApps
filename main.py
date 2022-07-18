d0d1aa642ba98ca8991e1f872643f20034bee151
robertay <94378311+robyuk@users.noreply.github.com>
2022-07-18 11:19:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('My first Streamlit App')
+st.subheader('Introducing Streamlit in Automating Everything With Python')
+st.write(''' This is my first Web App.
+         Enjoy it!!''')
